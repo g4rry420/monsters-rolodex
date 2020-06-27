4bf467e7d9628e1e87457293dde9d5c9ae56c5df
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-06-27 17:40:06 -0400

    Updates

---
+++ precache-manifest.ffd2360cd916fd010673fcc6294dd11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617787b9c2ad0ffe38db1b6148a76dd0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b02129b2eeedf878a5f",
+    "url": "/monsters-rolodex/static/css/main.768d73a0.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b02129b2eeedf878a5f",
+    "url": "/monsters-rolodex/static/js/main.4e744d5b.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
